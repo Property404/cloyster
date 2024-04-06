17dd5be1a902f6e55ddaa8267cdcff91b2a99100
Property404 <git@dagans.dev>
2024-04-06 18:23:07 -0500

    Press pause on thread locals
    Clearly I need to do some actually set up to get thread locals to work,
    but I don't to do that right now

--- src/errno.rs
+++ src/errno.rs
@@ -1,19 +1,23 @@
-use core::{cell::RefCell, ffi::c_int, fmt, num};
+use core::{cell::RefCell, ffi::c_int, fmt, num, ptr};
+use spin::Mutex;
 
 // The C standard only requires 3 error codes: EDOM, ERANGE, and EILSEQ
 // The POSIX standard
 
-#[thread_local]
-#[no_mangle]
-static errno: RefCell<c_int> = RefCell::new(0);
+static ERRNO: Mutex<RefCell<c_int>> = Mutex::new(RefCell::new(0));
 
 #[no_mangle]
-// This is unsafe because it returns the inner address so borrow checking is broken
-unsafe extern "C" fn __errno_location() -> *mut c_int {
-    core::ptr::addr_of_mut!(*errno.borrow_mut())
+// This is unsafe because it leaks the inner reference to errno,
+// which is not updated atomically
+// Really, we need thread_locals
+unsafe extern "C" fn __errno_location() -> *const c_int {
+    let errno = ERRNO.lock();
+    let ptr = ptr::from_ref(&*errno.borrow());
+    ptr
 }
 
 pub(crate) fn set_errno(val: Errno) {
+    let errno = ERRNO.lock();
     *errno.borrow_mut() = val.as_positive();
 }
 
