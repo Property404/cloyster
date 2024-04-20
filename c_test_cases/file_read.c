94602f4e81039cdffd7ffe0034706de2ce8f382b
Property404 <git@dagans.dev>
2024-04-19 23:32:31 -0500

    Add file read test

---
+++ c_test_cases/file_read.c
@@ -0,0 +1,22 @@
+// 12345
+// This program opens and reads itself
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+int main() {
+    printf("Opening %s\n", __FILE__);
+    FILE* fp = fopen(__FILE__, "rb");
+    printf("checking\n");
+    assert(fp != nullptr);
+
+    char buffer[9] = {0};
+    printf("Reading\n");
+    assert(fread(buffer, 1, sizeof(buffer) - 1, fp) == sizeof(buffer) - 1);
+
+    printf("Comparing\n");
+    assert(memcmp(buffer, "// 12345", sizeof(buffer)) == 0);
+
+    printf("OK\n");
+    return 0;
+}
